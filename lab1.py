868ecfc59cde3a1febafba4f3e133888baab0d42
Ryan Weinstein <116368220+RDWeinstein@users.noreply.github.com>
2024-08-27 10:59:17 -0500

    Update lab1.py

--- lab1.py
+++ lab1.py
@@ -3,20 +3,30 @@
 
 #1. Write a function reverse_and_uppercase(input_str) that takes a string, reverses it, and converts it to uppercase.
 def reverse_and_uppercase(input_str):
-    # Your code here
+    s = input_str[::-1]
+    return s.upper()
 
 assert reverse_and_uppercase("hello") == "OLLEH"
 assert reverse_and_uppercase("DataStructures") == "SERUTCURTSATAD"
 
 #2. Write a function basic_operations(a, b) that returns a tuple containing the sum, difference, product, and quotient of a and b.
 def basic_operations(a, b):
-    # Your code here
-
+    sum = a + b
+    difference = a-b
+    product = a*b
+    quotient = a/b
+    return (sum, difference, product, quotient)
+    
 assert basic_operations(10, 2) == (12, 8, 20, 5)
 
 #3. Write a function check_sign(number) that returns "Positive", "Negative", or "Zero" based on the input number.
 def check_sign(number):
-    # Your code here
+    if number > 0:
+        return 'Positive'
+    elif number < 0:
+        return 'Negative'
+    else:
+        return 'Zero'
 
 assert check_sign(5) == "Positive"
 assert check_sign(-3) == "Negative"
@@ -24,21 +34,33 @@ assert check_sign(0) == "Zero"
 
 #4. Write a function sum_of_evens(numbers) that returns the sum of all even numbers in a list.
 def sum_of_evens(numbers):
-    # Your code here
+    total = 0
+    for c in numbers:
+        if c % 2 == 0:
+            total += c
+    return total 
 
 assert sum_of_evens([1, 2, 3, 4, 5, 6]) == 12
 assert sum_of_evens([7, 8, 9, 10]) == 18
 
 #5. Write a recursive function fibonacci(n) that returns the nth Fibonacci number. Without recursion. Must use loop.
 def fibonacci(n):
-    # Your code here
+    if n <= 0:
+        return 0
+    num1 = 1
+    num2 = 1
+    for c in range(1, n-1, 1):
+        temp = num2+num1
+        num1 = num2
+        num2 = temp
+    return num2
 
 assert fibonacci(5) == 5
 assert fibonacci(10) == 55
 
 #6. Write a function parse_and_calculate(expression) that takes a mathematical expression in string format (e.g., "3+5*2-4/2") and returns the result. Handle the operations with the correct order of precedence.
 def parse_and_calculate(expression):
-    # Your code here
+    return eval(expression)
 
-assert parse_and_calculate("3+5*2-4/2") == 10
+assert parse_and_calculate("3+5*2-4/2") == 11
 assert parse_and_calculate("10-3*2+8/4") == 6
